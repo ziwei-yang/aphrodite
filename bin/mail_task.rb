00443101419ef18721b4d86abc528429e2784f41
zwyang <yangziwei@me.com>
2023-06-11 14:38:57 +0800

    spaced indentation; bitex patched

--- bin/mail_task.rb
+++ bin/mail_task.rb
@@ -4,211 +4,213 @@ require 'optparse/time'
 require 'ostruct'
 
 class MailTask
-	# Works on Linux with smtp service on.
-	# Otherwise it would try using GMAIL with account and password.
-	def self.email_plain(receiver, subject, content='EMPTY', bcc = nil, opt={})
-		hostname = ENV['HOSTNAME'] || "unknown.local"
-		author = (opt[:from] || "Automator <automator@#{hostname}>")
-		content ||= ""
-		content += File.read(opt[:html_file]) unless opt[:html_file].nil?
-		content = opt[:html] unless opt[:html].nil?
-		puts "email_plain -> #{receiver} | #{subject} | content:#{content.size} attachment:#{opt[:file] != nil}"
-		content = 'NO CONTENT' if content.empty?
-		mail = Mail.new do
-			to      receiver
-			from    author
-			subject subject
-			html_part do
-				content_type 'text/html; charset=UTF-8'
-				body content
-			end
-			unless opt[:file].nil?
-				files = opt[:file]
-				if files.is_a?(String)
-					files = files.split(",")
-					files = files.
-						map { |f| f.strip }.
-						select { |f| f.size > 0 }.
-						uniq
-				elsif files.is_a?(Array)
-					files = files.uniq
-				elsif files.nil?
-					files = []
-				else
-					abort "Files class error: #{files.class}"
-				end
-				files.each do |f|
-					add_file f
-				end
-			end
-		end
-		retry_ct = 0
-		begin
-			# If APD_MAIL_MODE is not set, guess.
-			if (ENV['APD_MAIL_MODE'] == 'smtp') || (ENV['APD_MAIL_MODE'].nil? && ENV['SMTP_USER'] != nil)
-				# Force mode selection smtp > gmail
-				puts "Sending email with smtp #{ENV["SMTP_USER"]}"
-				raise "No SMTP_USER/PSWD/DOMAIN/HOST in ENV" if ENV['SMTP_USER'].nil? || ENV['SMTP_PSWD'].nil? || ENV['SMTP_DOMAIN'].nil? || ENV['SMTP_HOST'].nil?
-				mail.from ENV['SMTP_USER']
-				smtp_settings = [:smtp, {
-						:address => ENV["SMTP_HOST"],
-						:port => 587,
-						:domain => ENV['SMTP_DOMAIN'],
-						:user_name => ENV['SMTP_USER'],
-						:password => ENV['SMTP_PSWD'],
-						:authentication => 'plain',
-						:enable_starttls_auto => true
-					}]
-				mail.delivery_method *smtp_settings
-			elsif (ENV['APD_MAIL_MODE'] == 'gmail') || (ENV['APD_MAIL_MODE'].nil? && ENV['GMAIL_USER'] != nil)
-				puts "Sending email with gmail smtp #{ENV["GMAIL_USER"]}"
-				raise "No GMAIL_USER & GMAIL_PSWD in ENV" if ENV['GMAIL_USER'].nil? || ENV['GMAIL_PSWD'].nil?
-				mail.from ENV['GMAIL_USER']
-				smtp_settings = [:smtp, {
-						:address => "smtp.gmail.com",
-						:port => 587,
-						:domain => 'gmail.com',
-						:user_name => ENV['GMAIL_USER'],
-						:password => ENV['GMAIL_PSWD'],
-						:authentication => 'plain',
-						:enable_starttls_auto => true
-					}]
-				mail.delivery_method *smtp_settings
-			else
-				raise "No known APD_MAIL_MODE set in ENV"
-			end
-			mail.deliver!
-		rescue Errno::ECONNREFUSED => e
-			if retry_ct < 1
-				puts "Try sending email again"
-				retry_ct += 1
-				retry
-			end
-			raise e
-		end
-	end
+  # Works on Linux with smtp service on.
+  # Otherwise it would try using GMAIL with account and password.
+  def self.email_plain(receiver, subject, content='EMPTY', bcc = nil, opt={})
+    hostname = ENV['HOSTNAME'] || "unknown.local"
+    author = (opt[:from] || "Automator <automator@#{hostname}>")
+    content ||= ""
+    content += File.read(opt[:html_file]) unless opt[:html_file].nil?
+    content = opt[:html] unless opt[:html].nil?
+    puts "email_plain -> #{receiver} | #{subject} | content:#{content.size} attachment:#{opt[:file] != nil}"
+    content = 'NO CONTENT' if content.empty?
+    mail = Mail.new do
+      to      receiver
+      from    author
+      subject subject
+      html_part do
+        content_type 'text/html; charset=UTF-8'
+        body content
+      end
+      unless opt[:file].nil?
+        files = opt[:file]
+        if files.is_a?(String)
+          files = files.split(",")
+          files = files.
+            map { |f| f.strip }.
+            select { |f| f.size > 0 }.
+            uniq
+        elsif files.is_a?(Array)
+          files = files.uniq
+        elsif files.nil?
+          files = []
+        else
+          abort "Files class error: #{files.class}"
+        end
+        files.each do |f|
+          add_file f
+        end
+      end
+    end
+    retry_ct = 0
+    begin
+      # If APD_MAIL_MODE is not set, guess.
+      if (ENV['APD_MAIL_MODE'] == 'smtp') || (ENV['APD_MAIL_MODE'].nil? && ENV['SMTP_USER'] != nil)
+        # Force mode selection smtp > gmail
+        puts "Sending email with smtp #{ENV["SMTP_USER"]}"
+        raise "No SMTP_USER/PSWD/DOMAIN/HOST in ENV" if ENV['SMTP_USER'].nil? || ENV['SMTP_PSWD'].nil? || ENV['SMTP_DOMAIN'].nil? || ENV['SMTP_HOST'].nil?
+        mail.from ENV['SMTP_USER']
+        smtp_settings = [:smtp, {
+            :address => ENV["SMTP_HOST"],
+            :port => 587,
+            :domain => ENV['SMTP_DOMAIN'],
+            :user_name => ENV['SMTP_USER'],
+            :password => ENV['SMTP_PSWD'],
+            :authentication => 'plain',
+            :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+            :enable_starttls_auto => true
+          }]
+        mail.delivery_method *smtp_settings
+      elsif (ENV['APD_MAIL_MODE'] == 'gmail') || (ENV['APD_MAIL_MODE'].nil? && ENV['GMAIL_USER'] != nil)
+        puts "Sending email with gmail smtp #{ENV["GMAIL_USER"]}"
+        raise "No GMAIL_USER & GMAIL_PSWD in ENV" if ENV['GMAIL_USER'].nil? || ENV['GMAIL_PSWD'].nil?
+        mail.from ENV['GMAIL_USER']
+        smtp_settings = [:smtp, {
+            :address => "smtp.gmail.com",
+            :port => 587,
+            :domain => 'gmail.com',
+            :user_name => ENV['GMAIL_USER'],
+            :password => ENV['GMAIL_PSWD'],
+            :authentication => 'plain',
+            :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,
+            :enable_starttls_auto => true
+          }]
+        mail.delivery_method *smtp_settings
+      else
+        raise "No known APD_MAIL_MODE set in ENV"
+      end
+      mail.deliver!
+    rescue Errno::ECONNREFUSED => e
+      if retry_ct < 1
+        puts "Try sending email again"
+        retry_ct += 1
+        retry
+      end
+      raise e
+    end
+  end
 
-	def self.email_mailgun(receiver, subject, content='EMPTY', bcc = nil, opt={})
-		require 'mailgun'
-		mailgun_key = ENV['MAILGUN_KEY'] || abort("No variable MAILGUN_KEY")
-		mailgun_site = ENV['MAILGUN_SITE'] || abort("No variable MAILGUN_SITE")
-		mg_client = Mailgun::Client.new mailgun_key
-		mb_obj = Mailgun::MessageBuilder.new()
-		# Define the from address
-		if opt[:shown_name].nil?
-			mb_obj.from("automator@#{mailgun_site}")
-		else
-			mb_obj.from("automator@#{mailgun_site}", {"first"=>opt[:shown_name]})
-		end
-		# Define a to recipient
-		receiver.split(',').each do |addr|
-			mb_obj.add_recipient(:to, addr.strip)
-		end
-		if bcc != nil
-			bcc.split(',').each do |addr|
-				mb_obj.add_recipient(:bcc, addr.strip)
-			end
-		end
-		# mb_obj.add_recipient(:cc, cc_addr)
-		# Define the subject
-		mb_obj.subject(subject || '')
-		html = nil
-		html = File.read(opt[:html_file]) unless opt[:html_file].nil?
-		html = opt[:html] unless opt[:html].nil?
-		mb_obj.body_html(html) unless html.nil?
-		unless opt[:file].nil?
-			files = opt[:file]
-			if files.is_a?(String)
-				files = files.split(",")
-				files = files.
-					map { |f| f.strip }.
-					select { |f| f.size > 0 }.
-					uniq
-			elsif files.is_a?(Array)
-				files = files.uniq
-			elsif files.nil?
-				files = []
-			else
-				abort "Files class error: #{files.class}"
-			end
-			files.each do |f| # Attach a file and rename it.
-				mb_obj.add_attachment f, File.basename(f)
-			end
-		end
-		# Need at least one of 'text' or 'html' parameters specified
-		content = 'NO CONTENT' if html.nil? && content.nil?
-		mb_obj.set_text_body(content) unless content.nil?
-		puts "email -> #{receiver} | #{subject} | content:#{(content||'').size} | html:#{(html||'').size} attachment:#{opt[:file] != nil}"
+  def self.email_mailgun(receiver, subject, content='EMPTY', bcc = nil, opt={})
+    require 'mailgun'
+    mailgun_key = ENV['MAILGUN_KEY'] || abort("No variable MAILGUN_KEY")
+    mailgun_site = ENV['MAILGUN_SITE'] || abort("No variable MAILGUN_SITE")
+    mg_client = Mailgun::Client.new mailgun_key
+    mb_obj = Mailgun::MessageBuilder.new()
+    # Define the from address
+    if opt[:shown_name].nil?
+      mb_obj.from("automator@#{mailgun_site}")
+    else
+      mb_obj.from("automator@#{mailgun_site}", {"first"=>opt[:shown_name]})
+    end
+    # Define a to recipient
+    receiver.split(',').each do |addr|
+      mb_obj.add_recipient(:to, addr.strip)
+    end
+    if bcc != nil
+      bcc.split(',').each do |addr|
+        mb_obj.add_recipient(:bcc, addr.strip)
+      end
+    end
+    # mb_obj.add_recipient(:cc, cc_addr)
+    # Define the subject
+    mb_obj.subject(subject || '')
+    html = nil
+    html = File.read(opt[:html_file]) unless opt[:html_file].nil?
+    html = opt[:html] unless opt[:html].nil?
+    mb_obj.body_html(html) unless html.nil?
+    unless opt[:file].nil?
+      files = opt[:file]
+      if files.is_a?(String)
+        files = files.split(",")
+        files = files.
+          map { |f| f.strip }.
+          select { |f| f.size > 0 }.
+          uniq
+      elsif files.is_a?(Array)
+        files = files.uniq
+      elsif files.nil?
+        files = []
+      else
+        abort "Files class error: #{files.class}"
+      end
+      files.each do |f| # Attach a file and rename it.
+        mb_obj.add_attachment f, File.basename(f)
+      end
+    end
+    # Need at least one of 'text' or 'html' parameters specified
+    content = 'NO CONTENT' if html.nil? && content.nil?
+    mb_obj.set_text_body(content) unless content.nil?
+    puts "email -> #{receiver} | #{subject} | content:#{(content||'').size} | html:#{(html||'').size} attachment:#{opt[:file] != nil}"
 
-		# Schedule message in the future
-		# mb_obj.set_delivery_time("tomorrow 8:00AM", "PST");
+    # Schedule message in the future
+    # mb_obj.set_delivery_time("tomorrow 8:00AM", "PST");
 
-		# Finally, send your message using the client
-		puts "Communicating with Mailgun."
-		result = mg_client.send_message(mailgun_site, mb_obj)
-		puts result.body
-	end
+    # Finally, send your message using the client
+    puts "Communicating with Mailgun."
+    result = mg_client.send_message(mailgun_site, mb_obj)
+    puts result.body
+  end
 end
 
 if __FILE__ == $0
-	# Send email by arguments
-	options = OpenStruct.new
-	options.banner = "Usage: [-v] -f from -r recipients -s subject -h html_file [-a attachment] "
-	OptionParser.new do |opts|
-		opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
-			options[:verbose] = v
-		end
-	
-		opts.on("-f", "--from address", "Email sender") do |v|
-			options[:sender] = v
-		end
-	
-		opts.on("-r", "--recipient recipients", "Email receiver") do |v|
-			options[:recipients] = v
-		end
-	
-		opts.on("-s", "--subject subject", "Email subject") do |v|
-			options[:subject] = v
-		end
+  # Send email by arguments
+  options = OpenStruct.new
+  options.banner = "Usage: [-v] -f from -r recipients -s subject -h html_file [-a attachment] "
+  OptionParser.new do |opts|
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+      options[:verbose] = v
+    end
 
-		opts.on("-c", "--content content", "Email content") do |v|
-			options[:content] = v
-		end
-	
-		opts.on("-h", "--html html-file", "Email content in HTML file") do |n|
-			options[:html_file] = n
-		end
-	
-		opts.on("-a", "--attachment file", "Attachment file") do |v|
-			options[:attachment] = v
-		end
-	
-		opts.on("-z", "--mailgun", "true/false") do |v|
-			options[:mailgun] = v
-		end
-	end.parse!
+    opts.on("-f", "--from address", "Email sender") do |v|
+      options[:sender] = v
+    end
 
-	abort "Subject missed, abort sending email" if options[:subject].nil?
-	abort "Recipients missed, abort sending email" if options[:recipients].nil?
-	if options[:mailgun] == true
-		MailTask.email_mailgun(
-			options[:recipients],
-			options[:subject],
-			options[:content],
-			nil,
-			html_file:options[:html_file],
-			file:options[:attachment],
-			from:options[:sender]
-		)
-	else
-		MailTask.email_plain(
-			options[:recipients],
-			options[:subject],
-			options[:content],
-			nil,
-			html_file:options[:html_file],
-			file:options[:attachment],
-			from:options[:sender]
-		)
-	end
+    opts.on("-r", "--recipient recipients", "Email receiver") do |v|
+      options[:recipients] = v
+    end
+
+    opts.on("-s", "--subject subject", "Email subject") do |v|
+      options[:subject] = v
+    end
+
+    opts.on("-c", "--content content", "Email content") do |v|
+      options[:content] = v
+    end
+
+    opts.on("-h", "--html html-file", "Email content in HTML file") do |n|
+      options[:html_file] = n
+    end
+
+    opts.on("-a", "--attachment file", "Attachment file") do |v|
+      options[:attachment] = v
+    end
+
+    opts.on("-z", "--mailgun", "true/false") do |v|
+      options[:mailgun] = v
+    end
+  end.parse!
+
+  abort "Subject missed, abort sending email" if options[:subject].nil?
+  abort "Recipients missed, abort sending email" if options[:recipients].nil?
+  if options[:mailgun] == true
+    MailTask.email_mailgun(
+      options[:recipients],
+      options[:subject],
+      options[:content],
+      nil,
+      html_file:options[:html_file],
+      file:options[:attachment],
+      from:options[:sender]
+    )
+  else
+    MailTask.email_plain(
+      options[:recipients],
+      options[:subject],
+      options[:content],
+      nil,
+      html_file:options[:html_file],
+      file:options[:attachment],
+      from:options[:sender]
+    )
+  end
 end
--- common/encode.rb
+++ common/encode.rb
@@ -1,64 +1,92 @@
 module EncodeUtil
-	def encode64(data)
-		Base64.encode64(data.nil? ? '':data).strip.gsub("\n", '')
-	end
+  def encode64(data)
+    Base64.encode64(data.nil? ? '':data).strip.gsub("\n", '')
+  end
 
-	def decode64(data)
-		return nil if data.nil? || data.empty?
-		Base64.decode64(data).force_encoding("UTF-8")
-	end
+  def decode64(data)
+    return nil if data.nil? || data.empty?
+    Base64.decode64(data).force_encoding("UTF-8")
+  end
 
-	def hash_str(data)
-		data.nil? ? Digest::MD5.hexdigest("") : Digest::MD5.hexdigest(data)
-	end
+  def hash_str(data)
+    data.nil? ? Digest::MD5.hexdigest("") : Digest::MD5.hexdigest(data)
+  end
 
-	def md5(data)
-		data.nil? ? Digest::MD5.hexdigest("") : Digest::MD5.hexdigest(data)
-	end
+  def md5(data)
+    data.nil? ? Digest::MD5.hexdigest("") : Digest::MD5.hexdigest(data)
+  end
 
-	def to_camel(snake, capFirst = false)
-		camel = nil
-		snake.split('_').each do |w|
-			if camel.nil?
-				camel = w.capitalize if capFirst
-				camel = w if !capFirst
-			else
-				camel << w.capitalize
-			end
-		end
-		camel
-	end
+  def to_camel(snake, capFirst = false)
+    camel = nil
+    snake.split('_').each do |w|
+      if camel.nil?
+        camel = w.capitalize if capFirst
+        camel = w if !capFirst
+      else
+        camel << w.capitalize
+      end
+    end
+    camel
+  end
 
-	def to_snake(camel)
-		camel.gsub(/::/, '/').
-		gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-		gsub(/([a-z\d])([A-Z])/,'\1_\2').
-		tr("-", "_").
-		downcase
-	end
+  def to_snake(camel)
+    camel.gsub(/::/, '/').
+      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      tr("-", "_").
+      downcase
+  end
+
+  # 12345678.1234 -> '  12,345,678.1234  '
+  def to_comma_num(num, intg_len, frac_len, opt={})
+    return " "*(intg_len+frac_len+1) if num.nil?
+    pstv = (num >= 0)
+    num = num.abs
+    frac = (num - num.to_i).round(frac_len)
+    frac_s = ''
+    frac_s = frac.to_s[1..-1] if frac != 0
+
+    # frac_s = '' or '.1234'
+    num_s = num.to_i.to_s
+    num_comma = ''
+    (num_s.size / 3 + 1).times { |i|
+      if 3*i+3 > num_s.size
+        num_comma = num_s[0..(-3*i-1)] + ',' + num_comma
+        break
+      else
+        num_comma = num_s[(-3*i-3)..(-3*i-1)] + ',' + num_comma
+      end
+    }
+    num_comma = num_comma[0..-2] if num_comma[-1] == ','
+    num_comma = num_comma[1..-1] if num_comma[0] == ','
+    num_comma = '-' + num_comma if !pstv
+    num_comma = opt[:prefix] + num_comma if opt[:prefix] != nil
+
+    str = num_comma.rjust(intg_len) + frac_s.ljust(frac_len+1)
+  end
 end
 
 module LZString
-	def lz_context
-		@lz_context ||= ExecJS.compile(File.read("#{APD_COMMON_PATH}/../res/lz-string-1.3.3-min.js"))
-	end
+  def lz_context
+    @lz_context ||= ExecJS.compile(File.read("#{APD_COMMON_PATH}/../res/lz-string-1.3.3-min.js"))
+  end
 
-	def lz_compressToBase64(string)
-		lz_context.call("LZString.compressToBase64", string)
-	end
+  def lz_compressToBase64(string)
+    lz_context.call("LZString.compressToBase64", string)
+  end
 
-	def lz_decompressFromBase64(string)
-		lz_context.call("LZString.decompressFromBase64", string)
-	end
+  def lz_decompressFromBase64(string)
+    lz_context.call("LZString.decompressFromBase64", string)
+  end
 end
 
 module ShortURLUtil
-	include EncodeUtil
-	def short_url(url, opt={})
-		return nil if url.nil?
-		raise "Url should be started with http/https" unless url.start_with?('http')
-		encode_url = encode64(url)
-		url = "http://dwz.wailian.work/api.php?url=#{encode_url}&site=sina"
-		response = curl url
-	end
+  include EncodeUtil
+  def short_url(url, opt={})
+    return nil if url.nil?
+    raise "Url should be started with http/https" unless url.start_with?('http')
+    encode_url = encode64(url)
+    url = "http://dwz.wailian.work/api.php?url=#{encode_url}&site=sina"
+    response = curl url
+  end
 end
